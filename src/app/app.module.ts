d6a0917394112db0a261e905a4b9eff0bb792666
FernandaEscobedo <mfernanda.escobedo@hotmail.com>
2019-05-30 20:21:46 -0500

    cambio de estilos en estacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 // Services
 import { BaseService } from './services/base.service';
 
-import { HttpClientModule} from "@angular/common/http";
+import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StationComponent } from './components/station/station.component';
